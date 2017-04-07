e3ff4d7438eab33b29de18ce220119b72f72220b
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-07 11:02:47 +0200

    After adding or removing a task new list is printed

--- src/ToDoAppMain.java
+++ src/ToDoAppMain.java
@@ -3,12 +3,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ToDoAppMain {
 
-  private final static String FILE_NAME = "data.csv";
+  private final static String FILE_NAME = "nodata.csv";
 
   public static void main(String[] args) {
 
@@ -19,18 +18,26 @@ public class ToDoAppMain {
               "Command line arguments:\n" +
               " -l   Lists all the tasks\n" +
               " -a   Adds a new task\n" +
-              " -r   Removes an task\n" +
-              " -c   Completes an task");
+              " -r   Removes a task\n" +
+              " -c   Completes a task");
     } else if (args[0].equals("-l") && readLinesFromFile().size() > 0) {
-        processData(readLinesFromFile());
+      processData(readLinesFromFile());
     } else if (args[0].equals("-l")) {
-        System.out.println("No todos for today! :)");
+      System.out.println("No todos for today! :)");
     } else if (args[0].equals("-a") && args.length == 2 ) {
-        writeToFile(addToList(args[1]));
-    } else if (args[0].equals("-a") && args.length == 1 ) {
-        System.out.println("Unable to add: no task provided");
-    } else if (args[0].equals("-r") ) {
-        writeToFile(removeTask(Integer.parseInt(args[1])));
+      writeToFile(addToList(args[1]));
+      processData(readLinesFromFile());
+    } else if (args[0].equals("-a") && args.length != 2 ) {
+      System.out.println("Unable to add: no task provided");
+    } else if (args[0].equals("-r") && args.length != 2 ) {
+      System.out.println("Unable to remove: no index provided");
+    } else if (args[0].equals("-r") && Integer.parseInt(args[1]) > readLinesFromFile().size()) {
+      System.out.println("Unable to remove: index is out of bound");
+    } else if (args[0].equals("-r") && args.length == 2 ) {
+      writeToFile(removeTask(Integer.parseInt(args[1])));
+      processData(readLinesFromFile());
+    } else if ((args[0].equals("-c"))) {
+      checkTask(Integer.parseInt(args[1]));
     }
 
   }
@@ -44,14 +51,13 @@ public class ToDoAppMain {
       e.printStackTrace();
       rawLines = new ArrayList<>();
     }
-
     return rawLines;
   }
 
   private static ArrayList<String> addToList(String newTask) {
     ArrayList <String> newTaskList = new ArrayList<>();
     newTaskList.addAll(readLinesFromFile());
-    newTaskList.add(newTask);
+    newTaskList.add( "[ ] " + newTask);
     return  newTaskList;
   }
 
@@ -71,6 +77,12 @@ public class ToDoAppMain {
     return newTaskList;
   }
 
+  private static void checkTask (int n) {
+    ArrayList <String> checkTaskList = new ArrayList<>();
+    checkTaskList.addAll(readLinesFromFile());
+    checkTaskList.get(n-1).replace(' ','x');
+    writeToFile(checkTaskList);
+  }
 
   private static void processData (List<String> rawLines) {
     for(int i = 0; i < rawLines.size(); i++) {
